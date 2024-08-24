23bd91a5567a76ff195fd21957bdb9cb1993bf3c
angelendrika <157441984+angelendrika@users.noreply.github.com>
2024-08-24 17:06:41 +0700

    Add files via upload

---
+++ dashboard/dashboard.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+sns.set(style='dark')
+
+def get_total_count_by_hour_df(hour_df):
+    hour_count_df = hour_df.groupby(by="hours").agg({"count_cr": ["sum"]})
+    return hour_count_df
+
+def macem_season(day_df): 
+    season_df = day_df.groupby(by="season").count_cr.sum().reset_index() 
+    return season_df
+
+days_df = pd.read_csv("https://raw.githubusercontent.com/angelendrika/bike-sharing/main/dashboard/day_clean.csv")
+hours_df = pd.read_csv("https://raw.githubusercontent.com/angelendrika/bike-sharing/main/dashboard/hour_clean.csv")
+
+datetime_columns = ["dteday"]
+days_df.sort_values(by="dteday", inplace=True)
+days_df.reset_index(inplace=True)   
+
+hours_df.sort_values(by="dteday", inplace=True)
+hours_df.reset_index(inplace=True)
+
+for column in datetime_columns:
+    days_df[column] = pd.to_datetime(days_df[column])
+    hours_df[column] = pd.to_datetime(hours_df[column])
+
+min_date_days = days_df["dteday"].min()
+max_date_days = days_df["dteday"].max()
+
+min_date_hour = hours_df["dteday"].min()
+max_date_hour = hours_df["dteday"].max()
+
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/image1_hH9B4gs.jpg")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value=min_date_days,
+        max_value=max_date_days,
+        value=[min_date_days, max_date_days])
+  
+main_df_days = days_df[(days_df["dteday"] >= str(start_date)) & 
+                       (days_df["dteday"] <= str(end_date))]
+
+main_df_hour = hours_df[(hours_df["dteday"] >= str(start_date)) & 
+                        (hours_df["dteday"] <= str(end_date))]
+
+hour_count_df = get_total_count_by_hour_df(main_df_hour)
+season_df = macem_season(main_df_hour)
+
+# Melengkapi Dashboard dengan Visualisasi Data
+
+st.header('Bike Sharing :sparkles:')
+
+st.subheader("Pada rentang waktu manakah permintaan sewa sepeda mencapai puncak dan titik terendahnya?")
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+
+sns.barplot(x="hours", y="count_cr", data=hour_count_df.sort_values(by=("count_cr", "sum"), ascending=False).head(5).reset_index(), palette=["#D3D3D3", "#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3"], ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Hours (PM)", fontsize=30)
+ax[0].set_title("Jam dengan banyak penyewa sepeda", loc="center", fontsize=30)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+
+sns.barplot(x="hours", y="count_cr", data=hour_count_df.sort_values(by=("count_cr", "sum"), ascending=True).head(5).reset_index(), palette=["#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3","#90CAF9"], ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Hours (AM)", fontsize=30)
+ax[1].set_title("Jam dengan sedikit penyewa sepeda", loc="center", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+
+st.subheader("Pada musim apa tingkat penggunaan layanan sewa sepeda cenderung meningkat?")
+colors = ["#D3D3D3", "#D3D3D3", "#D3D3D3", "#90CAF9"]
+fig, ax = plt.subplots(figsize=(20, 10))
+sns.barplot(
+    y="count_cr", 
+    x="season",
+    data=season_df.sort_values(by="count_cr", ascending=False),
+    palette=colors,
+    ax=ax
+)
+ax.set_title("Grafik Antar Musim", loc="center", fontsize=50)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='x', labelsize=35)
+ax.tick_params(axis='y', labelsize=30)
+st.pyplot(fig)
